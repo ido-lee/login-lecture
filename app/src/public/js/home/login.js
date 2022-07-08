92c552790263c5e51a464f1eb6fa9a5738ebcab9
ido.lee <idonggeon917@gmail.com>
2022-07-08 21:05:25 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,16 +1,38 @@
 "use strict";
 
+// login.ejs input태그의 id를 가져옴
 const id  = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     loginBtn = document.querySelector("button");
+    // login.ejs button을 가져옴 
 
+// console.log(id); --확인
+
+// login 버튼이 눌리고 click 이벤트 발생시 loging함수 실행
 loginBtn.addEventListener("click", login);
 
+// login 함수 실행
+// function login () { console.log("hello")} -- 확인 
+
+// console.log(id.value); -- 확인 id란에 입력한 정보가 들어옴
 function login( ) {
     const req = {
         id : id.value,
         psword : psword.value
     };
     
-    console.log(req);
+
+    // console.log(req); -- 확인  
+    // console.log(JSON.stringify(req)); -- 확인
+
+    // fetch 서버와 프론트와 어떤 경로에서 정보를 주고 받을지 정해줌
+    fetch("/login", {
+        method : "post",
+        headers : {
+            "content-Type" : "application/json"
+        },
+        // json 방식으로 전송 
+        // stringify : 문자 열로 바꿈
+        body : JSON.stringify(req)
+    });
 }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,8 +9,7 @@ const ctrl = require("./home.ctrl");
 
 // 요청이 '/'로 오면 ctrl에 hello를 실행
 router.get("/", ctrl.hello)
-
 router.get("/login", ctrl.login);
-
+    
 // 공유
 module.exports = router;
